cf0a2cf57009118237fbba1364ac2f84528dd5cb
Hong Truong Vinh <hongvinhkrn@gmail.com>
2022-05-10 15:24:14 +0700

    Hoan thanh cn gui mail co file text dinh kem

---
+++ NT106-LyThuyet_5-5/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿
+namespace NT106_LyThuyet_5_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_SignOut = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Sender = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.tb_Receiver = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.tb_Subject = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtb_MailBody = new System.Windows.Forms.RichTextBox();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.tb_Attachment = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.lb_Send = new System.Windows.Forms.Label();
+            this.btn_ShowPassword = new System.Windows.Forms.Label();
+            this.btn_Attach = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn_SignOut);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(72, 709);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.panel9);
+            this.panel2.Location = new System.Drawing.Point(91, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(563, 709);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.Controls.Add(this.panel8);
+            this.panel3.Controls.Add(this.panel7);
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(660, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(641, 709);
+            this.panel3.TabIndex = 2;
+            // 
+            // btn_SignOut
+            // 
+            this.btn_SignOut.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_SignOut.FlatAppearance.BorderSize = 0;
+            this.btn_SignOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(160)))), ((int)(((byte)(90)))));
+            this.btn_SignOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SignOut.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SignOut.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_SignOut.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_SignOut.Location = new System.Drawing.Point(3, 142);
+            this.btn_SignOut.Name = "btn_SignOut";
+            this.btn_SignOut.Size = new System.Drawing.Size(66, 55);
+            this.btn_SignOut.TabIndex = 13;
+            this.btn_SignOut.Text = "X";
+            this.btn_SignOut.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_SignOut.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(160)))), ((int)(((byte)(90)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 55);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "+";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Người gửi:";
+            // 
+            // tb_Sender
+            // 
+            this.tb_Sender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Sender.Location = new System.Drawing.Point(130, 2);
+            this.tb_Sender.Name = "tb_Sender";
+            this.tb_Sender.Size = new System.Drawing.Size(376, 30);
+            this.tb_Sender.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.tb_Sender);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(634, 41);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btn_ShowPassword);
+            this.panel5.Controls.Add(this.tb_Password);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(4, 51);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(634, 41);
+            this.panel5.TabIndex = 3;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(130, 2);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(376, 30);
+            this.tb_Password.TabIndex = 1;
+            this.tb_Password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.tb_Receiver);
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Location = new System.Drawing.Point(4, 98);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(634, 41);
+            this.panel6.TabIndex = 3;
+            // 
+            // tb_Receiver
+            // 
+            this.tb_Receiver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Receiver.Location = new System.Drawing.Point(130, 2);
+            this.tb_Receiver.Name = "tb_Receiver";
+            this.tb_Receiver.Size = new System.Drawing.Size(376, 30);
+            this.tb_Receiver.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Người nhận:";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.rtb_MailBody);
+            this.panel7.Controls.Add(this.lb_Send);
+            this.panel7.Controls.Add(this.tb_Subject);
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.Location = new System.Drawing.Point(3, 192);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(631, 514);
+            this.panel7.TabIndex = 4;
+            // 
+            // tb_Subject
+            // 
+            this.tb_Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Subject.Location = new System.Drawing.Point(93, 6);
+            this.tb_Subject.Multiline = true;
+            this.tb_Subject.Name = "tb_Subject";
+            this.tb_Subject.Size = new System.Drawing.Size(474, 57);
+            this.tb_Subject.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Subject:";
+            // 
+            // rtb_MailBody
+            // 
+            this.rtb_MailBody.Location = new System.Drawing.Point(8, 69);
+            this.rtb_MailBody.Name = "rtb_MailBody";
+            this.rtb_MailBody.Size = new System.Drawing.Size(620, 442);
+            this.rtb_MailBody.TabIndex = 4;
+            this.rtb_MailBody.Text = "";
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.btn_Attach);
+            this.panel8.Controls.Add(this.tb_Attachment);
+            this.panel8.Controls.Add(this.label5);
+            this.panel8.Location = new System.Drawing.Point(4, 145);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(634, 41);
+            this.panel8.TabIndex = 4;
+            // 
+            // tb_Attachment
+            // 
+            this.tb_Attachment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Attachment.Location = new System.Drawing.Point(130, 2);
+            this.tb_Attachment.Name = "tb_Attachment";
+            this.tb_Attachment.Size = new System.Drawing.Size(375, 30);
+            this.tb_Attachment.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Đính kèm:";
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.Info;
+            this.panel9.Location = new System.Drawing.Point(4, 9);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(556, 83);
+            this.panel9.TabIndex = 0;
+            // 
+            // lb_Send
+            // 
+            this.lb_Send.AutoSize = true;
+            this.lb_Send.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.lb_Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Send.Location = new System.Drawing.Point(573, 22);
+            this.lb_Send.Name = "lb_Send";
+            this.lb_Send.Size = new System.Drawing.Size(42, 25);
+            this.lb_Send.TabIndex = 2;
+            this.lb_Send.Text = "Gửi";
+            this.lb_Send.Click += new System.EventHandler(this.lb_Send_Click);
+            // 
+            // btn_ShowPassword
+            // 
+            this.btn_ShowPassword.AutoSize = true;
+            this.btn_ShowPassword.BackColor = System.Drawing.Color.Transparent;
+            this.btn_ShowPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ShowPassword.Location = new System.Drawing.Point(512, 5);
+            this.btn_ShowPassword.Name = "btn_ShowPassword";
+            this.btn_ShowPassword.Size = new System.Drawing.Size(33, 25);
+            this.btn_ShowPassword.TabIndex = 3;
+            this.btn_ShowPassword.Text = "👁";
+            this.btn_ShowPassword.Click += new System.EventHandler(this.btn_ShowPassword_Click);
+            // 
+            // btn_Attach
+            // 
+            this.btn_Attach.AutoSize = true;
+            this.btn_Attach.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_Attach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Attach.Location = new System.Drawing.Point(511, 5);
+            this.btn_Attach.Name = "btn_Attach";
+            this.btn_Attach.Size = new System.Drawing.Size(60, 25);
+            this.btn_Attach.TabIndex = 3;
+            this.btn_Attach.Text = "Chọn";
+            this.btn_Attach.Click += new System.EventHandler(this.btn_Attach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 734);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_SignOut;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox tb_Receiver;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox tb_Sender;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox tb_Subject;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtb_MailBody;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.TextBox tb_Attachment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label lb_Send;
+        private System.Windows.Forms.Label btn_ShowPassword;
+        private System.Windows.Forms.Label btn_Attach;
+    }
+}
+
---
+++ NT106-LyThuyet_5-5/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NT106_LyThuyet_5_5
+{
+    public partial class Form1 : Form
+    {
+        Attachment attachment = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tb_Receiver.Text = "hongvinhkrn@gmail.com";
+            tb_Sender.Text = "hongvinh45krn@gmail.com";
+
+            tb_Subject.Text = "Test Sending Mail";
+            rtb_MailBody.Text = "This is body";
+        }
+
+        bool SendMyMail(string sender, string password, string receiver, string mailSubject, string mailBody, Attachment attachment)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(sender);
+                    mail.To.Add(receiver);
+                    mail.Subject = mailSubject;
+                    mail.Body = mailBody;
+                    mail.Attachments.Add(attachment);
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new System.Net.NetworkCredential(sender, password);
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        string SetFormatMail(string bodyMail)
+        {
+            string myBodyMail = "<h1>" + bodyMail + "</h1>";
+            return myBodyMail;
+        }
+
+        private void lb_Send_Click(object sender, EventArgs e)
+        {
+            string senderMail = tb_Sender.Text;
+            //string password = tb_Password.Text;
+            string password = "vjnh2002";
+            string receiver = tb_Receiver.Text;
+            string bodyMail = SetFormatMail(rtb_MailBody.Text);
+            string subjectMail = tb_Subject.Text;
+
+            if(SendMyMail(senderMail, password, receiver, subjectMail, bodyMail, attachment) == true)
+            {
+                MessageBox.Show("Đã gửi thành công");
+            }
+            else
+            {
+                MessageBox.Show("Lỗi!!! Gửi không thành công");
+            }
+        }
+
+        bool tb_showPassword = false;
+        private void btn_ShowPassword_Click(object sender, EventArgs e)
+        {
+            if (tb_showPassword)
+            {
+                tb_Password.UseSystemPasswordChar = false;
+                tb_showPassword = false;
+            }
+            else
+            {
+                tb_Password.UseSystemPasswordChar = true;
+                tb_showPassword = true;
+            }
+        }
+
+        private void btn_Attach_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "|*.txt;*.docx;*.rtf;*.pdf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                attachment = new Attachment(openFileDialog.FileName);
+            }
+        }
+    }
+}
